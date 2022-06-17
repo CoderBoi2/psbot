bede87630e488d238b3876a3a552a637ac57c324
CoderBoi2 <103612036+CoderBoi2@users.noreply.github.com>
2022-06-17 17:12:14 +1000

    Delete Commands directory

--- Commands/backroom.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = (client, message, args) => {
-    message.channel.send("backrooms is a **scary** game in **roblox** which looks like the **end** in **mincraft**.")
-    }
-    
-    exports.name = "backroom"
--- Commands/ping.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = (client, message, args) => {
-message.channel.send("pong")
-}
-
-exports.name = "ping"
